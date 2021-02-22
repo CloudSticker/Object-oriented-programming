93cad84c121c3ec9301c2a1a88d2285b48a0b078
Cloud Maker <dan9154634874@gmail.com>
2021-02-22 15:58:30 +0300

    Добавление 1й лабораторной работы

---
+++ Лабораторные работы/Лабораторная 1/Задание 1 часть 2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Второе_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+
+            Int32.TryParse(Console.ReadLine(), out a);
+            Int32.TryParse(Console.ReadLine(), out b);
+
+
+            if (a > 0)
+            {
+                Console.WriteLine(Math.Max(a, b));
+            }
+            else
+            {
+                Console.WriteLine(Math.Min(a, b));
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ЗАПУСКАЕМ
+░ГУСЯ░▄▀▀▀▄░РАБОТЯГИ░░
+▄███▀░◐░░░▌░░░░░░░
+░░░░▌░░░░░▐░░░░░░░
+░░░░▐░░░░░▐░░░░░░░
+░░░░▌░░░░░▐▄▄░░░░░
+░░░░▌░░░░▄▀▒▒▀▀▀▀▄
+░░░▐░░░░▐▒▒▒▒▒▒▒▒▀▀▄
+░░░▐░░░░▐▄▒▒▒▒▒▒▒▒▒▒▀▄
+░░░░▀▄░░░░▀▄▒▒▒▒▒▒▒▒▒▒▀▄
+░░░░░░▀▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▀▄
+░░░░░░░░░░░▌▌░▌▌░░░░░
+░░░░░░░░░░░▌▌░▌▌░░░░░
+░░░░░░░░░▄▄▌▌▄▌▌░░░░░
+
+*/
+
+
+
+
+
